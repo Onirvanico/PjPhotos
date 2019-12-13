4d598aab00af4178ad066219e8fcfc8415966fc5
Cristiano <cristianosantosesilva@gmail.com>
2019-12-13 15:24:06 -0300

    Várias alterações feitas, nem sei quanto

---
+++ src/app/photos/photo-list/photo-list.resolver.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Photo } from '../photo/photo.interface';
+import { PhotoService } from '../photo/photo.service';
+
+@Injectable({providedIn: "root"})
+export class PhotoListResolver implements Resolve<Observable<Photo[]>{
+    
+    constructor(private photoService: PhotoService) {}
+
+    resolve(route: ActivatedRouteSnapshot, state:RouterStateSnapshot): Observable<Photo[]> | Observable<Observable<Photo[]>> | Promise<Observable<Photo[]>> {
+          const userName = route.params.userName;
+          return this.photoService.listFromUserPaginated(userName, 1);
+    }
+
+}
