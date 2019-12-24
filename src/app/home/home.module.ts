dc3c9bee8e944e21d1cf23eeefd732d6347f879b
Cristiano <cristianosantosesilva@gmail.com>
2019-12-24 13:29:56 -0300

    Component signup sendo criado

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,15 +1,17 @@
 
 import { SignInComponent } from './signin/signin.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { VmessageModule } from '../shared/components/vmessage/vmessage.module';
 import { RouterModule } from '@angular/router';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
-    declarations: [ SignInComponent ],
+    declarations: [ SignInComponent, SignupComponent ],
     imports: [ ReactiveFormsModule,
                CommonModule,
+               FormsModule,
                VmessageModule,
                RouterModule 
             ]
